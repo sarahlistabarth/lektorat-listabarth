0c6a3e7d8de3178172af17a4f8163d0bbeb450a0
szeller42 <s.zeller@posteo.net>
2023-03-24 19:03:00 +0100

    🔐 rename secret

--- get_github_code.R
+++ get_github_code.R
@@ -17,7 +17,7 @@ scrape_macro <- function(macro_name) {
   macro_script <-
     GET(
       url = url,
-      authenticate("s.zeller@posteo.net", Sys.getenv("github_at")),    
+      authenticate("s.zeller@posteo.net", Sys.getenv("at")),    
       accept("application/vnd.github.v3.raw")
     ) |> 
     content(as = "text")
