c9104bdbe9e611f5e2070319f14d061a132a87b5
szeller42 <s.zeller@posteo.net>
2023-03-24 18:36:43 +0100

    ✨ scrape macro code

---
+++ get_github_code.R
@@ -0,0 +1,50 @@
+library(httr)
+library(dotenv)
+library(here)
+
+if (!"macros" %in% list.files()) {
+  dir.create("macros")
+}
+
+# Source R script from Github
+scrape_macro <- function(macro_name) {
+  url <- paste0("https://api.github.com/repos/szeller42/Word-macros/contents/",
+                macro_name,
+                "().vb",
+                collapse = "")
+  file_name <- paste0("macros/", macro_name, ".vb",
+                      collapse = "")
+  macro_script <-
+    GET(
+      url = url,
+      authenticate("s.zeller@posteo.net", Sys.getenv("github_at")),    
+      accept("application/vnd.github.v3.raw")
+    ) |> 
+    content(as = "text")
+  
+  write(macro_script,
+        file_name)
+}
+
+macro_names <- c("AddM",
+                 "AddN",
+                 "AddR",
+                 "AddS",
+                 "Bindestrich",
+                 "Compositum",
+                 "Compositum_ohne_des",
+                 "FindAndDeleteRFC",
+                 "HoldMySpot",
+                 "MarkierungenLoeschen",
+                 "MoveToEndOfWord",
+                 "SatzendeMarkieren",
+                 "StopCombinationsAddEntry",
+                 "Synonyme",
+                 "countTC",
+                 "deleteLastCharOfWord",
+                 "geschuetztesLeerzeichen",
+                 "swapSingularPlural",
+                 "ZitatKlammernZuText"
+                 # "toggleUmlaut"
+                 )
+sapply(macro_names, scrape_macro)
